85c942e0f0de237099a15b2e9d6560d31eae65f4
Grabt234 <gnorrie12@gmail.com>
2023-09-11 21:42:28 +0200

    Chore: moving TCP Handle connection into ChunkCPPGoAdapter

--- ChunkCPPGoAdapter/ChunkCPPGoAdapter.go
+++ ChunkCPPGoAdapter/ChunkCPPGoAdapter.go
@@ -3,6 +3,11 @@ package ChunkCPPGoAdapter
 
 import (
 	"encoding/binary"
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/rs/zerolog"
 )
 
 /*
@@ -10,6 +15,99 @@ getSessionStates is a partial implementation to extract transmission states of T
 
 returns [transmissionState, sessionNumber, sequenceNumber, transmissionSize]
 */
+
+func HandleConnectionTCPIncomingChunkTypes(dataChannel chan<- string, conn net.Conn) {
+	defer conn.Close()
+
+	logger := zerolog.New(os.Stderr).With().Timestamp().Logger()
+
+	// Create a buffer to read incoming data
+	buffer := make([]byte, 512)
+	var byteArray []byte
+	var JSONByteArray []byte
+
+	previousSessionNumber := uint32(0)
+	previousSequenceNumber := uint32(0)
+	sessionContinuous := false
+	newSequence := false
+	LastInSequence := false
+
+	for {
+
+		// Read data from the connection into the buffer
+		bytesRead, err := conn.Read(buffer)
+		if err != nil {
+			logger.Error().Msg("Error reading:" + err.Error())
+			break
+		}
+
+		byteArray = append(byteArray, buffer[:bytesRead]...)
+
+		// check if byte array is large enough
+		if len(byteArray) > 4096 {
+
+			// Expected byte Format
+			// |Transport Header(2)| [Session Header(23)|Session Data(x)] |
+
+			// Lets first check how many bytes in the transport layer message
+			TransportLayerHeaderSize := 2
+			TransportLayerDataSize := binary.LittleEndian.Uint16(byteArray[:TransportLayerHeaderSize])
+			logger.Info().Msg("TransportLayerDataSize:" + fmt.Sprint(TransportLayerDataSize))
+
+			// The carry on and extract session state information (v1.0.0 of chunk types)
+			SessionLayerHeaderSize := 23
+			transmissionSize := TransportLayerDataSize
+			TCPHeaderBytes := byteArray[TransportLayerHeaderSize : SessionLayerHeaderSize+TransportLayerHeaderSize]
+			transmissionState, sessionNumber, sequenceNumber := ConvertBytesToSessionStates(TCPHeaderBytes)
+			logger.Info().Msg("States: Transmission State " + string(transmissionState) +
+				" Session Number " + fmt.Sprint(sessionNumber) +
+				" Sequence Number " + fmt.Sprint(sequenceNumber) +
+				" Transmission Size " + fmt.Sprint(transmissionSize))
+
+			// Now we check if the Session in continuous
+			sessionContinuous, newSequence, LastInSequence, previousSessionNumber, previousSequenceNumber =
+				CheckSessionContinuity(transmissionState, sessionNumber, sequenceNumber, previousSessionNumber, previousSequenceNumber)
+			logger.Info().Msg("States: sessionContinuous " + fmt.Sprint(sessionContinuous) +
+				" newSequence " + fmt.Sprint(newSequence) +
+				" LastInSequence " + fmt.Sprint(LastInSequence))
+
+			if newSequence {
+
+				JSONStartIndex := GetJSONStartIndex()
+
+				JSONByteArray = byteArray[TransportLayerHeaderSize+SessionLayerHeaderSize+JSONStartIndex : transmissionSize]
+
+			} else if sessionContinuous && !LastInSequence {
+
+				JSONStartIndex := GetJSONStartIndex()
+
+				JSONByteArray = append(JSONByteArray,
+					byteArray[TransportLayerHeaderSize+SessionLayerHeaderSize+JSONStartIndex:transmissionSize]...)
+
+			} else if sessionContinuous && LastInSequence {
+
+				JSONStartIndex := GetJSONStartIndex()
+
+				JSONByteArray = append(JSONByteArray,
+					byteArray[TransportLayerHeaderSize+SessionLayerHeaderSize+JSONStartIndex:transmissionSize]...)
+
+				str := string(JSONByteArray)
+				//logger.Info().Msg(str)
+				dataChannel <- str
+
+				JSONByteArray = nil
+			} else {
+				JSONByteArray = nil
+			}
+
+			byteArray = byteArray[TransportLayerDataSize:]
+
+		}
+	}
+
+	fmt.Printf("Connection from %s closed\n", conn.RemoteAddr())
+}
+
 func ConvertBytesToSessionStates(byteArray []byte) (byte, uint32, uint32) {
 
 	index := 0
--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"encoding/binary"
-	"fmt"
-	"log"
 	"net"
 	"net/http"
 	"os"
@@ -21,124 +18,37 @@ import (
 
 func main() {
 
-	//logger := zerolog.New(os.Stderr).With().Timestamp().Logger()
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go handleConnectionWebSocketOutgoing(&wg)
-	wg.Wait()
-	// // Define the port to listen on
-	// port := "10005"
-
-	// // Create a TCP listener on the specified port
-	// listener, err := net.Listen("tcp", ":"+port)
-	// if err != nil {
-	// 	logger.Fatal().Msg("Error:" + err.Error())
-	// 	os.Exit(1)
-	// }
-	// defer listener.Close()
-
-	// logger.Info().Msg("TCP server is listening on port:" + port)
-
-	// // Accept incoming connections
-	// for {
-	// 	conn, err := listener.Accept()
-	// 	if err != nil {
-	// 		logger.Error().Msg("Error:" + err.Error())
-	// 		continue
-	// 	}
-	// 	go handleConnectionTCPIncoming(conn)
-	// }
-}
-
-func handleConnectionTCPIncoming(conn net.Conn) {
-	defer conn.Close()
+	dataChannel := make(chan string) // Create an integer channel
 
 	logger := zerolog.New(os.Stderr).With().Timestamp().Logger()
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go handleConnectionWebSocketOutgoing(dataChannel, &wg)
 
-	// Create a buffer to read incoming data
-	buffer := make([]byte, 512)
-	var byteArray []byte
-	var JSONByteArray []byte
+	// Define the port to listen on
+	port := "10005"
 
-	previousSessionNumber := uint32(0)
-	previousSequenceNumber := uint32(0)
-	sessionContinuous := false
-	newSequence := false
-	LastInSequence := false
+	// Create a TCP listener on the specified port
+	listener, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		logger.Fatal().Msg("Error:" + err.Error())
+		os.Exit(1)
+	}
+	defer listener.Close()
 
+	logger.Info().Msg("TCP server is listening on port:" + port)
+
+	// Accept incoming connections
 	for {
-
-		// Read data from the connection into the buffer
-		bytesRead, err := conn.Read(buffer)
+		conn, err := listener.Accept()
 		if err != nil {
-			logger.Error().Msg("Error reading:" + err.Error())
-			break
+			logger.Error().Msg("Error:" + err.Error())
+			continue
 		}
-
-		byteArray = append(byteArray, buffer[:bytesRead]...)
-
-		// check if byte array is large enough
-		if len(byteArray) > 4096 {
-
-			// Expected byte Format
-			// |Transport Header(2)| [Session Header(23)|Session Data(x)] |
-
-			// Lets first check how many bytes in the transport layer message
-			TransportLayerHeaderSize := 2
-			TransportLayerDataSize := binary.LittleEndian.Uint16(byteArray[:TransportLayerHeaderSize])
-			logger.Info().Msg("TransportLayerDataSize:" + fmt.Sprint(TransportLayerDataSize))
-
-			// The carry on and extract session state information (v1.0.0 of chunk types)
-			SessionLayerHeaderSize := 23
-			transmissionSize := TransportLayerDataSize
-			TCPHeaderBytes := byteArray[TransportLayerHeaderSize : SessionLayerHeaderSize+TransportLayerHeaderSize]
-			transmissionState, sessionNumber, sequenceNumber := ChunkCPPGoAdapter.ConvertBytesToSessionStates(TCPHeaderBytes)
-			logger.Info().Msg("States: Transmission State " + string(transmissionState) +
-				" Session Number " + fmt.Sprint(sessionNumber) +
-				" Sequence Number " + fmt.Sprint(sequenceNumber) +
-				" Transmission Size " + fmt.Sprint(transmissionSize))
-
-			// Now we check if the Session in continuous
-			sessionContinuous, newSequence, LastInSequence, previousSessionNumber, previousSequenceNumber =
-				ChunkCPPGoAdapter.CheckSessionContinuity(transmissionState, sessionNumber, sequenceNumber, previousSessionNumber, previousSequenceNumber)
-			logger.Info().Msg("States: sessionContinuous " + fmt.Sprint(sessionContinuous) +
-				" newSequence " + fmt.Sprint(newSequence) +
-				" LastInSequence " + fmt.Sprint(LastInSequence))
-
-			if newSequence {
-
-				JSONStartIndex := ChunkCPPGoAdapter.GetJSONStartIndex()
-
-				JSONByteArray = byteArray[TransportLayerHeaderSize+SessionLayerHeaderSize+JSONStartIndex : transmissionSize]
-
-			} else if sessionContinuous && !LastInSequence {
-
-				JSONStartIndex := ChunkCPPGoAdapter.GetJSONStartIndex()
-
-				JSONByteArray = append(JSONByteArray,
-					byteArray[TransportLayerHeaderSize+SessionLayerHeaderSize+JSONStartIndex:transmissionSize]...)
-
-			} else if sessionContinuous && LastInSequence {
-
-				JSONStartIndex := ChunkCPPGoAdapter.GetJSONStartIndex()
-
-				JSONByteArray = append(JSONByteArray,
-					byteArray[TransportLayerHeaderSize+SessionLayerHeaderSize+JSONStartIndex:transmissionSize]...)
-
-				str := string(JSONByteArray)
-				logger.Info().Msg(str)
-
-				JSONByteArray = nil
-			} else {
-				JSONByteArray = nil
-			}
-
-			byteArray = byteArray[TransportLayerDataSize:]
-
-		}
+		go ChunkCPPGoAdapter.HandleConnectionTCPIncomingChunkTypes(dataChannel, conn)
 	}
 
-	fmt.Printf("Connection from %s closed\n", conn.RemoteAddr())
+	wg.Wait()
 }
 
 var upgrader = websocket.Upgrader{
@@ -146,7 +56,7 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
-func handleConnectionWebSocketOutgoing(wg *sync.WaitGroup) {
+func handleConnectionWebSocketOutgoing(dataChannel <-chan string, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	logger := zerolog.New(os.Stderr).With().Timestamp().Logger()
@@ -172,14 +82,17 @@ func handleConnectionWebSocketOutgoing(wg *sync.WaitGroup) {
 
 		conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
 		if err != nil {
-			log.Println(err)
+			logger.Error().Msg(err.Error())
 			return
 		}
 		defer conn.Close()
 		for {
-			conn.WriteMessage(websocket.TextMessage, []byte("Hello, WebSocket!"))
+			println("wohoo-----<")
+			dataString := <-dataChannel
+			println("wohoo-----<<")
+			conn.WriteMessage(websocket.TextMessage, []byte(dataString))
 			time.Sleep(time.Second)
-			println("wohoo")
+			logger.Debug().Msg("Transmitting Websocket data")
 		}
 	})
 	router.Run(":10010")
