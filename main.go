5e414f750fbcfc7ef57a1bc0c9b89729491b354f
Grabt234 <gnorrie12@gmail.com>
2023-09-11 18:20:22 +0200

    Feat: Adding basic websocket implementation

--- main.go
+++ main.go
@@ -3,39 +3,51 @@ package main
 import (
 	"encoding/binary"
 	"fmt"
+	"log"
 	"net"
+	"net/http"
 	"os"
+	"sync"
 
 	ChunkCPPGoAdapter "github.com/Sense-Scape/Go_TCP_Websocket_Adapter/v2/ChunkCPPGoAdapter"
 	"github.com/rs/zerolog"
+
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 )
 
 func main() {
 
-	logger := zerolog.New(os.Stderr).With().Timestamp().Logger()
+	//logger := zerolog.New(os.Stderr).With().Timestamp().Logger()
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go handleConnectionWebSocketOutgoing(&wg)
+	wg.Wait()
+	// // Define the port to listen on
+	// port := "10005"
 
-	// Define the port to listen on
-	port := "10005"
+	// // Create a TCP listener on the specified port
+	// listener, err := net.Listen("tcp", ":"+port)
+	// if err != nil {
+	// 	logger.Fatal().Msg("Error:" + err.Error())
+	// 	os.Exit(1)
+	// }
+	// defer listener.Close()
 
-	// Create a TCP listener on the specified port
-	listener, err := net.Listen("tcp", ":"+port)
-	if err != nil {
-		logger.Fatal().Msg("Error:" + err.Error())
-		os.Exit(1)
-	}
-	defer listener.Close()
+	// logger.Info().Msg("TCP server is listening on port:" + port)
 
-	logger.Info().Msg("TCP server is listening on port:" + port)
-
-	// Accept incoming connections
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			logger.Error().Msg("Error:" + err.Error())
-			continue
-		}
-		go handleConnectionTCPIncoming(conn)
-	}
+	// // Accept incoming connections
+	// for {
+	// 	conn, err := listener.Accept()
+	// 	if err != nil {
+	// 		logger.Error().Msg("Error:" + err.Error())
+	// 		continue
+	// 	}
+	// 	go handleConnectionTCPIncoming(conn)
+	// }
 }
 
 func handleConnectionTCPIncoming(conn net.Conn) {
@@ -129,6 +141,47 @@ func handleConnectionTCPIncoming(conn net.Conn) {
 	fmt.Printf("Connection from %s closed\n", conn.RemoteAddr())
 }
 
-func handleConnectionWebSocketOutgoing() {
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func handleConnectionWebSocketOutgoing(wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	logger := zerolog.New(os.Stderr).With().Timestamp().Logger()
+	logger.Info().Msg("TransportLayerDataSize:")
+	println("wohoo-----")
+
+	router := gin.Default()
+
+	// CORS middleware setup
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowOrigins = []string{"http://localhost:5173/"} // Replace with your SvelteKit frontend URL
+
+	corsGroup := router.Group("/private")
+	corsGroup.Use(cors.New(corsConfig))
+
+	// Allow all origins to connect
+	// Note that is is not safe
+	upgrader.CheckOrigin = func(r *http.Request) bool {
+		return true
+	}
+
+	router.GET("/public", func(c *gin.Context) {
+
+		conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		defer conn.Close()
+		for {
+			conn.WriteMessage(websocket.TextMessage, []byte("Hello, WebSocket!"))
+			time.Sleep(time.Second)
+			println("wohoo")
+		}
+	})
+	router.Run(":10010")
 
 }
