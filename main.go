98de41525c11c8dab557dfbb7adb120a4c92d48e
Grabt234 <gnorrie12@gmail.com>
2023-10-22 19:45:23 +0200

    Feat: Porting websocket routine to zerlog

--- main.go
+++ main.go
@@ -41,8 +41,11 @@ func main() {
 	go Routines.HandleLogging(routineCompleteChannel, serverConfigStringMap, LoggingChannel)
 
 	routineCount = routineCount + 1
-	GenericChunkChannel := make(chan string) // Create an integer channel
+	GenericChunkChannel := make(chan string)
 	go Routines.HandleTCPReceivals(LoggingChannel, GenericChunkChannel)
+
+	routineCount = routineCount + 1
+	go Routines.HandleWebSocketChunkTransmissions(LoggingChannel, GenericChunkChannel)
 
 	for {
 		time.Sleep(1 * time.Second)
@@ -53,35 +56,4 @@ func main() {
 		LoggingChannel <- myMap
 
 	}
-
-	// for i := 1; i <= routineCount; i++ {
-	// 	<-routineCompleteChannel
-	// }
-
-	// // Define the TCP port to listen on
-	// port := "10100"
-	// // Create a channel to pass time chunk json docs around
-	// TimeChunkDataChannel := make(chan string) // Create an integer channel
-
-	// // Create a TCP listener on the specified port
-	// listener, err := net.Listen("tcp", ":"+port)
-	// if err != nil {
-	// 	logger.Fatal().Msg("Error:" + err.Error())
-	// 	os.Exit(1)
-	// }
-	// defer listener.Close()
-	// logger.Info().Msg("TCP server is listening on port:" + port)
-
-	// // Accept incoming TCP connections
-	// for {
-	// 	conn, err := listener.Accept()
-	// 	if err != nil {
-	// 		logger.Error().Msg("Error:" + err.Error())
-	// 		continue
-	// 	}
-	// 	// Start up TCP and websocket threads
-	// 	go Routines.HandleTCPReceivals(TimeChunkDataChannel, conn)
-	// 	//go Routines.HandleWebSocketTimeChunkTransmissions(TimeChunkDataChannel, "/DataTypes/TimeChunk")
-	// }
-
 }
